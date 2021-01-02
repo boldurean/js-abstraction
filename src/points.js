cd806793727fa0f71ad097fe728d60ad5b7f70e0
vsbdn <v.boldurean@gmail.com>
2021-01-02 18:35:15 +0000

    remove: points/test add: segments.js/ segments.test.js

--- src/points.js
+++ src/points.js
@@ -1,16 +1,8 @@
-export default (point1, point2) => {
-  const [x1, y1] = point1;
-  const [x2, y2] = point2;
-  return Math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2));
-};
-// alternative
+export default (x, y) => ({
+  x,
+  y,
+});
 
-const calculateDistance = (point1, point2) => {
-  const deltaX = point2[0] - point1[0];
-  const deltaY = point2[1] - point1[1];
-  return Math.hypot(deltaX, deltaY);
-};
+export const getX = (point) => point.x;
 
-const point1 = [0, 0];
-const point2 = [3, 4];
-console.log(calculateDistance(point1, point2));
+export const getY = (point) => point.y;
---
+++ src/segments.js
@@ -0,0 +1,31 @@
+import makeDecartPoint from './points.js';
+
+const makeSegment = (beginPoint, endPoint) => ({
+  beginPoint,
+  endPoint,
+});
+
+const getMidpointOfSegment = (segment) => {
+  const x = (segment.beginPoint.x + segment.endPoint.x) / 2;
+  const y = (segment.beginPoint.y + segment.endPoint.y) / 2;
+  return { x, y };
+};
+
+const getBeginPoint = (segment) => segment.beginPoint;
+
+const getEndPoint = (segment) => segment.endPoint;
+
+export {
+  makeSegment,
+  getMidpointOfSegment,
+  getBeginPoint,
+  getEndPoint,
+};
+
+const beginPoint = makeDecartPoint(3, 2);
+const endPoint = makeDecartPoint(0, 0);
+const segment = makeSegment(beginPoint, endPoint);
+
+getMidpointOfSegment(segment); // (1.5, 1)
+getBeginPoint(segment); // (3, 2)
+getEndPoint(segment); // (0, 0)
